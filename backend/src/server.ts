f1cec80fddcea44c03dcf35a2f2bc6a05e52e4ae
Anderson Melo <142186594+AndersonMeloo@users.noreply.github.com>
2025-10-28 22:54:46 -0300

    feat: npm i, cors, express

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,22 @@
-import { Router } from "express";
+import express, { Router } from "express";
+import cors from "cors";
 
-export const router = Router()
+const app = express();
+const router = Router();
 
-router.get('./test', (req, res) = {
+// Permitir qualquer origem (localhost:3000,4000,5000....)
+app.use(cors());
 
-    res.json({ name: 'GTI' })
-})
+// Rotas
+router.get('/test', (req, res) => {
+    res.json({ name: "GTI" });
+});
+
+app.use('/', router);
+
+app.listen(3000, () => console.log('Server running on port 3000'));
+
+// npm install express
+// npm install --save-dev @types/express ts-node typescript4
+// npm install cors
+// npm install --save-dev @types/cors
